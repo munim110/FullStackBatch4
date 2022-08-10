8b1fca4a934b479550d437df94dd90e4140a667d
Tanveer Hossain Munim <munim987654321@gmail.com>
2022-08-10 22:02:42 +0600

    first commit

---
+++ Student/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    phone = models.IntegerField()
+    address = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='student_photos')
